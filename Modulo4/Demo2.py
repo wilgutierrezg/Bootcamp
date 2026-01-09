d21070c3578b446fb3e8fc9df9c2c506825a1a6a
wgutierrezg <wilgutierrezg@gmail.com>
2026-01-09 06:45:19 -0600

    Clase4

---
+++ Modulo4/Demo2.py
@@ -0,0 +1,9 @@
+import Demo
+
+inventario_2 = [
+    {"nombre": "manzanas", "precio": 25, "stock": 100},
+    {"nombre": "peras", "precio": 10, "stock": 200},
+    {"nombre": "melon", "precio": 70, "stock": 10}
+]
+
+print(Demo.formatear_nombres(inventario_2))
